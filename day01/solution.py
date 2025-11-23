dd405debb83efb12da3544aa2185580b97a1ec91
hiromi-in <72237080+hiromi-in@users.noreply.github.com>
2025-11-22 22:04:11 -0600

    Enhance data analysis with new prints and plots
    Added additional print statements for column names and product category counts. Included histograms for price and quantity.

--- day01/solution.py
+++ day01/solution.py
@@ -8,3 +8,13 @@ df = pd.read_csv('/kaggle/input/sales-100daysofchallenge/sales_10000.csv')
 print(df)
 print(df.describe())
 print(df.info())
+print(df.columns)
+print(df['product_category'].value_counts())
+
+plt.subplot(1, 2, 1)
+plt.hist(df['price'])
+plt.xlabel('Price')
+
+plt.subplot(1, 2, 2)
+plt.hist(df['quantity'])
+plt.xlabel('Quantity')
